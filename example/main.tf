9a95446f6ce9d6d4dfdd5e14a04ea691526575f0
VijayKotecha <vijay@DESKTOP-4S01FPN>
2023-03-17 10:15:16 +0000

    Update code and example. The code is updated to not send repeat emails if the key is already inactive

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,9 @@
 module "aws_iam_advisor" {
   source       = "KOTechnologiesLtd/iam-advisor/aws"
-  version      = "1.0.0"
+  version      = "1.0.2"
   ses_arn      = "arn:aws:ses:eu-west-2:XXXXXXXXXXXXXX:identity/XXXX@XXXXXXX.co.uk"
   sender_email = "XXXX@XXXXXXX.co.uk"
   admin_email  = "XXXX@XXXXXXX.co.uk"
   dryrun       = "false"
+  iam_to_exclue = "\"example\",\"example\""
 }
